ca857cb08243e2e9b6e93da3f0ac6566445957a8
Pine Mizune <pinemz@gmail.com>
2022-11-06 16:47:02 +0900

    test: add tests for equals

--- src/test/java/moe/pine/spring/util/NamedByteArrayResourceTest.java
+++ src/test/java/moe/pine/spring/util/NamedByteArrayResourceTest.java
@@ -1,19 +1,49 @@
 package moe.pine.spring.util;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ByteArrayResource;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class NamedByteArrayResourceTest {
-    @Test
-    void init() {
-        final byte[] bytes = {0x01, 0x02, 0x03};
-        final String filename = "abc.txt";
-        final ByteArrayResource resource = new NamedByteArrayResource(bytes, filename);
+    @Nested
+    class Initialize {
+        @Test
+        void success() {
+            final byte[] bytes = {0x01, 0x02, 0x03};
+            final String filename = "abc.txt";
+            final ByteArrayResource resource = new NamedByteArrayResource(bytes, filename);
 
-        assertEquals(filename, resource.getFilename());
-        assertArrayEquals(bytes, resource.getByteArray());
+            assertEquals(filename, resource.getFilename());
+            assertArrayEquals(bytes, resource.getByteArray());
+        }
+    }
+
+    @Nested
+    class Equals {
+        @Test
+        void equality() {
+            final byte[] bytes = {0x01, 0x02, 0x03};
+            final String filename = "abc.txt";
+            final ByteArrayResource resource1 = new NamedByteArrayResource(bytes, filename);
+            final ByteArrayResource resource2 = new NamedByteArrayResource(bytes, filename);
+
+            assertEquals(resource1, resource1);
+            assertEquals(resource1, resource2);
+        }
+
+        @Test
+        void inequality() {
+            final byte[] bytes = {0x01, 0x02, 0x03};
+            final String filename1 = "abc.txt";
+            final String filename2 = "bcd.txt";
+            final ByteArrayResource resource1 = new NamedByteArrayResource(bytes, filename1);
+            final ByteArrayResource resource2 = new NamedByteArrayResource(bytes, filename2);
+
+            assertNotEquals(resource1, resource2);
+        }
     }
 }
