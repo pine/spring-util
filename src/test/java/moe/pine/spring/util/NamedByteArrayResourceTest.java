05adb900ad9bcf5b11a29b5cc05a0b42c59660a0
Pine Mizune <pinemz@gmail.com>
2022-11-06 16:52:19 +0900

    test: add test for NamedByteArrayResource

--- src/test/java/moe/pine/spring/util/NamedByteArrayResourceTest.java
+++ src/test/java/moe/pine/spring/util/NamedByteArrayResourceTest.java
@@ -36,14 +36,38 @@ class NamedByteArrayResourceTest {
         }
 
         @Test
-        void inequality() {
+        void differentByteArray() {
+            final byte[] bytes1 = {0x01, 0x02, 0x03};
+            final byte[] bytes2 = {0x01, 0x02, 0x04};
+            final String filename = "abc.txt";
+            final ByteArrayResource resource1 = new NamedByteArrayResource(bytes1, filename);
+            final ByteArrayResource resource2 = new NamedByteArrayResource(bytes2, filename);
+
+            assertNotEquals(resource1, resource2);
+        }
+        @Test
+        void differentFilename() {
             final byte[] bytes = {0x01, 0x02, 0x03};
             final String filename1 = "abc.txt";
-            final String filename2 = "bcd.txt";
+            final String filename2 = "abd.txt";
             final ByteArrayResource resource1 = new NamedByteArrayResource(bytes, filename1);
             final ByteArrayResource resource2 = new NamedByteArrayResource(bytes, filename2);
 
             assertNotEquals(resource1, resource2);
+        }
+    }
+
+    @Nested
+    class HashCode {
+        @Test
+        void equality() {
+            final byte[] bytes = {0x01, 0x02, 0x03};
+            final String filename = "abc.txt";
+            final ByteArrayResource resource1 = new NamedByteArrayResource(bytes, filename);
+            final ByteArrayResource resource2 = new NamedByteArrayResource(bytes, filename);
+
+            assertEquals(resource1.hashCode(), resource1.hashCode());
+            assertEquals(resource1.hashCode(), resource2.hashCode());
         }
     }
 }
